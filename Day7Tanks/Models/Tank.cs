80032812d094be5b0859337e97f5a9f802af4c5d
Nastya <anastasya.0702@mail.ru>
2018-04-26 19:15:51 +0600

    Добавьте файлы проекта.

---
+++ Day7Tanks/Models/Tank.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Tanks.Models
+{
+    enum TypeOfTank {T34, Pantera }
+    class Tank
+    {
+        /// <summary>
+        /// Боекомлект
+        /// </summary>
+        private int Ammunition { get; set; }
+        /// <summary>
+        /// Уровень брони
+        /// </summary>
+        private int Armor { get; set; }
+        /// <summary>
+        /// Уровень маневренности
+        /// </summary>
+        private int Maneuverability { get; set; }
+        public TypeOfTank TankType { get; set; }
+        public Tank() { }
+        public Tank(TypeOfTank Tank)
+        {
+            Random random = new Random();
+            Ammunition = random.Next(0, 100);
+            Armor = random.Next(0, 100);
+            Maneuverability = random.Next(0, 100);
+            TankType = Tank;
+        }
+
+        public static Tank operator *(Tank tank1, Tank tank2)
+        {
+            Tank winner = new Tank();
+            if (tank1.Ammunition > tank2.Ammunition &&
+                tank1.Armor > tank2.Armor)
+            {
+                winner = tank1;
+            }
+            else if (tank1.Maneuverability > tank2.Maneuverability &&
+                     tank1.Ammunition > tank2.Ammunition)
+            {
+                winner = tank1;
+            }
+            else if (tank1.Maneuverability > tank2.Maneuverability &&
+                     tank1.Armor > tank2.Armor)
+            {
+                winner = tank1;
+            }
+            else winner = tank2;
+
+            return winner;
+        }
+
+        public override string ToString()
+        {
+            string info = string.Format("Тип: {0} (Боекомплект : {1} Уровень брони : {2} Уровень маневренности : {3})" , 
+                this.TankType.ToString(), this.Ammunition, this.Armor, this.Maneuverability);
+            return info;
+        }
+    }
+}
---
+++ Day7Tanks/War.cs
@@ -0,0 +1,31 @@
+﻿using Day7Tanks.Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace Day7Tanks
+{
+    public class War
+    {
+        List<Tank> team1;
+        List<Tank> team2;
+
+        public War()
+        {
+            team1 = new List<Tank>();
+            team2 = new List<Tank>();
+        }
+
+        public static War operator < (List<Tank> team1, List<Tank> team2)
+        {
+            return
+        }
+        public static War operator >(List<Tank> team1, List<Tank> team2)
+        {
+            return
+        }
+
+
+
+    }
+}
